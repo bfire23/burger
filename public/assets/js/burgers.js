11da16952b5e77131e5171fd07ebe69b6f00bbf9
Blake Arambula <bfire23@gmail.com>
2020-09-03 09:41:39 -0700

    setting up burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,45 +1,39 @@
 $(function() {
-    $(".change-sleep").on("click", function(event) {
-      const id = $(this).data("id");
-      const newSleep = $(this).data("newsleep");
-  
-      const newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burger/:id" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      const newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("api/burger/", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var eaten = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eaten
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
   });
-  
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burg").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("Inserted new Burger");
+       
+        location.reload();
+      }
+    );
+  });
+});
